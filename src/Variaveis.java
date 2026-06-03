cde8a3b5d1111dff96665d00801623ce7c5b618f
Vitor Lyra <vkaczmarzykly@gmail.com>
2026-06-03 18:47:25 -0300

    feat/tipos-primitivos feat: Adicionando exercicio de tipos premitivos

---
+++ src/Variaveis.java
@@ -0,0 +1,14 @@
+public class Variaveis {
+
+    public static void main(String[] args) {
+        String nome = "Vitor";
+        int idade = 33;
+        double altura = 1.79;
+        boolean estudante = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("É estudante: " + estudante);
+    }
+}
