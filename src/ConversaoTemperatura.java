7da7d5f56660b5fa4c5dee662ce16b1cb4b6097d
Vitor Lyra <vkaczmarzykly@gmail.com>
2026-06-03 18:59:33 -0300

    feat: Adicionando o exercicio de conversao de temperatura.

---
+++ src/ConversaoTemperatura.java
@@ -0,0 +1,9 @@
+public class ConversaoTemperatura {
+    static void main() {
+        int celsius = 20;
+
+        double fahrenheit = (double)(celsius * 9 / 5) + 32;
+
+        System.out.println("A temperatura em graus Fahrenheit é: " + fahrenheit);
+    }
+}
