37896b719311a26797302d451f45a592031d3afe
Vitor Lyra <vkaczmarzykly@gmail.com>
2026-06-09 04:33:37 -0300

    feat/par-ou-impar (feat): Adicionando o exercicio Par ou Impar.

---
+++ src/ParImpar.java
@@ -0,0 +1,14 @@
+public class ParImpar {
+    static void main() {
+
+        int numero = 2023;
+        int resultado = numero % 2;
+
+        if (resultado == 0) {
+            System.out.println("O numero é par");
+        } else {
+            System.out.println("O numero é impar");
+        }
+
+    }
+}
